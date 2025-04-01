67a39597dea761dbc734c3ad45ee7dc02dafbd2a
si0na <148415179+si0na@users.noreply.github.com>
2025-04-01 11:01:07 +0530

    Delete travel_request directory

--- travel_request/travelapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Employee,Admin,Manager,TravelRequest
-
-admin.site.register(Employee)
-admin.site.register(Admin)
-admin.site.register(Manager)
-admin.site.register(TravelRequest)
